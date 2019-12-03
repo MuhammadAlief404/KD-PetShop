ef9f106fb26560dd379553ce308e03b4bddfd802
Muhammad Alief <alief.pribadi@gmail.com>
2019-12-03 16:39:45 +0700

    - login Finish

--- app/src/main/java/com/quantumhiggs/kdpetshop/ui/LoginFragment.kt
+++ app/src/main/java/com/quantumhiggs/kdpetshop/ui/LoginFragment.kt
@@ -2,15 +2,14 @@ package com.quantumhiggs.kdpetshop.ui
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.quantumhiggs.kdpetshop.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class LoginFragment : Fragment() {
 
     override fun onCreateView(
@@ -21,5 +20,25 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        btn_login.setOnClickListener {
+            when {
+                edt_username_login.text.isNullOrEmpty() -> edt_username_login.error =
+                    getString(R.string.error_empty_username)
+                edt_password_login.text.isNullOrEmpty() -> edt_password_login.error =
+                    getString(R.string.error_empty_password)
+                edt_username_login.text.length < 6 -> edt_username_login.error =
+                    getString(R.string.error_length_username)
+                edt_password_login.text.length < 6 -> edt_password_login.error =
+                    getString(R.string.error_length_password)
+                else -> {
+                    val direction =
+                        LoginFragmentDirections.actionToHome(edt_username_login.text.toString())
+                    it.findNavController().navigate(direction)
+                }
+            }
+        }
+    }
 }
